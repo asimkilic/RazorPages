eaf2b38a96e9fb5d16ad13b6d2851db494c7a8fa
asimkilic <a.asim.kilic@gmail.com>
2022-02-11 11:11:00 +0000

    Create page created

--- AbbyWeb/Data/ApplicationDbContext.cs
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -3,8 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AbbyWeb.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
         public DbSet<Category> Categories { get; set; }
     }
 }
